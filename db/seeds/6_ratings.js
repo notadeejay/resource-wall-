90bcdf18b9c38af7cef90b88fede4f943792e559
Sean <leoseanquilty@gmail.com>
2017-08-16 20:07:40 +0000

    Syntax correction

--- db/seeds/6_ratings.js
+++ db/seeds/6_ratings.js
@@ -5,18 +5,18 @@ exports.seed = function(knex, Promise) {
         user_id: 900,
         resource_id: 900,
         rating: 5
-      });
+      }),
     knex('ratings').insert(
       {
         user_id: 901,
         resource_id: 901,
         rating: 5
-      });
+      }),
     knex('ratings').insert(
       {
         user_id: 902,
         resource_id: 902,
         rating: 5
-      });
-  ])
-}
+      })
+  ]);
+};
