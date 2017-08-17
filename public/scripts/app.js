07c87f9440194243073b2fd44129bd6e7c04450e
vadimgavrish <vadim@gavrish.ca>
2017-08-17 18:15:35 +0000

    added responsive grid

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,14 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+
+  $('.grid').isotope({
+    layoutMode: 'cellsByRow',
+    itemSelector: '.grid-item',
+    cellsByRow: {
+      columnWidth: 520,
+      rowHeight: 520
+    }
+  });
 });
