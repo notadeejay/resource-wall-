8839eb6860f20cf1cba9993a1073e9135dfe09aa
Amy <amy@lighthouselabs.ca>
2017-08-17 22:17:48 +0000

    a

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,101 @@
+const generateHTML = (obj) => {
+  const html = `
+      <div class ="grid-item">
+        <article>
+                <div class = 'articleHeader'>
+                    <h5>${obj.title}</h5>
+                </div>
+                <div class = 'articleBody'>
+                  <p class = "description"> ${obj.description}</p>
+                </div>
+                <div class = 'articleFooter'>
+                   <p class = 'url'>${obj.url}</p>
+                </footer>
+            </article>
+        </div>`
+  return html
+}
+
+
+
 $(() => {
-  $.ajax({
+
+//REGISTRATION HANDLER
+$("#registrationform").on("submit", function (event) {
+    let $this = $(this)
+    let data = $(this).serialize()
+    event.preventDefault();
+    $.ajax({
+        url: "/api/users/register",
+        method: "POST",
+        data: data,
+         }).then(function (result) {
+          window.location.href = "/new"
+
+        });
+});
+
+//LOGIN HANDLER
+  $("#loginform").on("submit", function(event) {
+    let data = $(this).serialize()
+    let $this = $(this)
+    event.preventDefault();
+    $.ajax({
+        url: "/api/users/login",
+        method: "POST",
+        data: data,
+         }).then(function (result) {
+
+          window.location.href = "/resources"
+  });
+});
+
+//LOGOUT HANDLER
+$(".logoutbutton").on("click", function (event) {
+  event.preventDefault();
+  $.ajax ({
+    url: "/api/users/logout",
+    method: "POST"
+  }).then(function (result) {
+    window.location.href = "/"
+  });
+});
+
+const loadResources = () => {
+$.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
+    url: "/api/resources/resources"
+  }).done((resources) => {
+
+     renderResources(resources)
+
   });;
+}
+
+$('.grid').isotope({
+    layoutMode: 'cellsByRow',
+    itemSelector: '.grid-item',
+    cellsByRow: {
+      columnWidth: 390,
+      rowHeight: 390
+    }
+  });
+
+
+  $('#searchButton').click(function() {
+    $('#searchBar').animate({width: 'toggle'});
+  })
+
+ const renderResources = (data) => {
+  $('#grid').html('');
+    let html = data
+              .sort((a,b) => b.id - a.id)
+              .map(generateHTML)
+              .join('')
+    $('#grid').html(html)
+  }
+
+loadResources();
+
+
 });
--- routes/resources.js
+++ routes/resources.js
@@ -2,65 +2,36 @@
 
 const express = require('express');
 const router  = express.Router();
-const db = require('../lib/util/queries.js');
 
 module.exports = (knex) => {
 
-//GET ALL RESOURCES
-router.get("/resources", (req, res) => {
-     knex
-      .select("*")
+
+  router.post("/", function (req, res) {
+
+    let newResource = {
+      title: req.body.title,
+      description: req.body.description,
+      url: req.body.url,
+      user_id: req.session.user
+    }
+
+       knex('resources')
+      .insert(newResource)
+      .then( (results) => {
+       res.status(200).redirect('/resources')
+      });
+
+  });
+
+router.get("/resources", function (req, res) {
+  knex.select("*")
       .from("resources")
       .then((results) => {
         res.json(results);
+
     });
-  });
+ });
 
-  //ADD NEW RESOURCE TO DATABASE
-  router.post("/", function (req, res) {
-   knex.addNewResource({
-      title: req.body.title
-      description: req.body.description
-      url: req.body.url
-      user_id: req.session.id
-    }, () => {
-      res.redirect('/resources');
-  });
-
-  //DELETE A RESOURCE
-  router.delete("/:resource_id", function (req,res){
-
-  });
-
-  //GET ALL RESOURCES FOR USER
-  router.get("/resources", function (req,res) {
-    let user = req.session.user.id
-
-    knex.getAllResourcesForUser(user, (urls) => {
-        res.render('myresources', {
-          urls: urls
-        });
-    });
-  });
-
- //LIKE A RESOURCE
-  router.post("/:resource_id/like", function (req, res) {
-   let resourceID = req.params.resource_id
-   let userID = req.session.id
-    knex.addLikes(resourceID, userID, (likes) => {
-      res.render('resources', {likes: likes
-      });
-    })
-
-  })
-
-  //UNLIKE A RESOURCE
-  router.delete("/", (req, res) => {
-
-    knex.removeLike(req, (likes) => {
-
-    }
-  })
 
 return router
 
