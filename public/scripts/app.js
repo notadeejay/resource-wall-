0dde4dbc84f93ab3847c5dbfb67cb8a68fab4e5a
Amy <amy@lighthouselabs.ca>
2017-08-17 20:02:29 +0000

    Toggle navbar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,38 @@
 $(() => {
+
+//REGISTRATION HANDLER
+$("#registrationform").on("submit", function (event) {
+    let $this = $(this)
+    let data = $(this).serialize()
+    event.preventDefault();
+    $.ajax({
+        url: "/api/users/register",
+        method: "POST",
+        data: data,
+         }).then(function (result) {
+          window.location.href = "/new"
+
+        });
+});
+
+//LOGIN HANDLER
   $("#loginform").on("submit", function(event) {
     let data = $(this).serialize()
+    let $this = $(this)
     event.preventDefault();
     $.ajax({
         url: "/api/users/login",
         method: "POST",
         data: data,
          }).then(function (result) {
-          window.location.href = "/resources"
-  });
-});
+           window.location.href = "/resources"
 
 
-});
+         });
+   });
+
+ });
+
+
+
+
