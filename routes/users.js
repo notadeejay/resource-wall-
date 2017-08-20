5b5b9b8d7faa7f2fadf83d5878f740b78b70e73e
Amy <amy@lighthouselabs.ca>
2017-08-20 20:49:00 +0000

    Cleaned up users.js

--- routes/users.js
+++ routes/users.js
@@ -6,117 +6,146 @@ const bcrypt = require('bcrypt');
 
 module.exports = (knex) => {
 
-router.get("/", function(req, res) {
-  knex.select("id", "first_name")
-  .from("users")
-  .where("id", req.session.user)
-  .then((result) => {
-    res.json(result)
 
-}).catch(function(error) {
-    console.log(error);
+ //GET CURRENT USER
+  router.get("/", function(req, res) {
+    knex.select("id", "first_name")
+    .from("users")
+    .where("id", req.session.user)
+    .then((result) => {
+      res.json(result)
+   }).catch(function(error) {
+
+      console.log(error);
+
+     });
+
   });
+
+
+  //ADD NEW USER TO DATABASE @ REGISTRATION
+  router.post("/register", function (req, res) {
+
+    if (!req.body.first_name || !req.body.last_name) {
+      return res.status(400).send(`You must provide a first and last name.`)}
+    if (!req.body.email) {
+      return res.status(400).send(`You must provide an email address.`)}
+    if (!req.body.password) {
+      return res.status(400).send(`You must create a password.`)}
+    if (req.body.password != req.body.password_confirmation) {
+      return res.status(400).send(`Passwords do not match.`)}
+
+    let firstname = req.body.first_name
+    let lastname  = req.body.last_name
+    let email     = req.body.email
+    let password  = bcrypt.hashSync(req.body.password, 10)
+
+    let newUser = {
+            first_name: firstname,
+            last_name:  lastname,
+            email:      email,
+            password:   password
+          }
+
+     knex('users')
+    .insert(newUser)
+    .returning(['id', 'first_name'])
+    .then((result) => {
+
+      req.session.user = result[0].id;
+      req.session.name = result[0].first_name
+      res.status(200).redirect('/resources')
+
+    }).catch(function(error) {
+
+      console.log(error);
+     });
 
 });
 
 
+//LOGIN
+  router.post("/login", function (req, res) {
+    let emailReq = req.body.email
+    let passwordReq = req.body.password
 
-//ADD NEW USER TO DATABASE @ REGISTRATION
-router.post("/register", function (req, res) {
-
-  if (!req.body.first_name || !req.body.last_name) {
-    return res.status(400).send(`You must provide a first and last name.`)}
-  if (!req.body.email) {
-    return res.status(400).send(`You must provide an email address.`)}
-  if (!req.body.password) {
-    return res.status(400).send(`You must create a password.`)}
-  if (req.body.password != req.body.password_confirmation) {
-    return res.status(400).send(`Passwords do not match.`)}
-
-  let firstname = req.body.first_name
-  let lastname = req.body.last_name
-  let email = req.body.email
-  let password = bcrypt.hashSync(req.body.password, 10)
-
-  let newUser = {
-          first_name: firstname,
-          last_name: lastname,
-          email: email,
-          password: password
-      }
+    if(!emailReq || !passwordReq) {
+      res.status(403).send(`Must enter valid username and password`)
+    }
 
        knex('users')
-      .insert(newUser)
-      .returning(['id', 'first_name'])
-      .then((result) => {
-        req.session.user = result[0].id;
-        req.session.name = result[0].first_name
-        res.status(200).redirect('/resources')
-      })
+      .select('password', 'id', 'first_name')
+      .where({'email' : emailReq})
+      .then(function(result) {
 
-  });
+        //CHECK IF USER EXISTS
+        if (!result || !result[0])  {
+          res.status(404).send(`User not found`)
+        }
 
-router.post("/login", function (req, res) {
-  const emailReq = req.body.email
-  const passwordReq = req.body.password
-  if(!emailReq || !passwordReq) {
-    res.status(403).send('Must enter valid username and password')
-  }
-  knex('users')
-  .select('password', 'id', 'first_name')
-  .where({'email' : emailReq})
-  .then(function(result) {
-    if (!result || !result[0])  {  // NOT FOUND!
-      return;
-    }
-    var pass = result[0].password;
-    if(bcrypt.compareSync(passwordReq, pass)){
-     req.session.user = result[0].id
-     req.session.name = result[0].name
-     res.status(200).redirect("/resources")
+        //COMPARE PASSWORDS
+        if(bcrypt.compareSync(passwordReq, result[0].password)){
+           req.session.user = result[0].id
+           req.session.name = result[0].name
+           res.status(200).redirect('/resources')
 
-     console.log('Success')
-   } else {
-    console.log('Failed login')
-  }
-})
-  .catch(function(error) {
-    console.log(error);
-  });
-})
+        } else {
 
- router.post("/logout", (req, res) => {
+          res.status(401).send(`Unauthorized`)
+
+        }
+      }).catch(function(error) {
+
+      console.log(error);
+
+    });
+
+  })
+
+
+
+
+  //LOGOUT
+  router.post('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect("/");
+    res.redirect('/');
   });
+
+
+
 
   //EDIT USER PROFILE
-    router.put("/edit", function (req, res) {
+  router.put('/edit', function (req, res) {
 
-      if (req.body.password != "") {
-        if (req.body.password === req.body.password_confirmation) {
-          let newpassword = bcrypt.hashSync(req.body.password, 10)
-        }
-          else {
-            alert(`Passwords do not match!`)
-          }
-        }
+    if (req.body.password != "") {
+      if (req.body.password === req.body.password_confirmation) {
+        let newpassword = bcrypt.hashSync(req.body.password, 10)
+      } else {
+          alert(`Passwords do not match!`)
+      }
+    }
 
-        const editUser = {
-          first_name: req.body.first_name || undefined,
-          last_name: req.body.last_name || undefined,
-          email: req.body.email || undefined,
-          password: newpassword || undefined
-        }
 
-           knex('users')
-          .where('id', req.session.user)
-          .update(editUser)
-          .then((result) => {
-            console.log(result)
-            res.status(200).redirect("/resources")
-          })
+    const editUser = {
+      first_name: req.body.first_name || undefined,
+      last_name:  req.body.last_name || undefined,
+      email:      req.body.email || undefined,
+      password:   newpassword || undefined
+    }
+
+
+      knex('users')
+      .where('id', req.session.user)
+      .update(editUser)
+      .then((result) => {
+
+        res.status(200).redirect("/resources")
+      }).catch(function(error) {
+
+      console.log(error);
+
       });
+  });
 
   return router;
 }
