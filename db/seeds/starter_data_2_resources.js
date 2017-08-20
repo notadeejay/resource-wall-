b71147cbbbc805b5f514c4f1348706368029c06a
Sean <leoseanquilty@gmail.com>
2017-08-20 03:22:53 +0000

    Tweaked the seeding data

--- db/seeds/starter_data_2_resources.js
+++ db/seeds/starter_data_2_resources.js
@@ -7,27 +7,35 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('resources').insert(
           {
-            title: 'The Goog',
-            description: 'Source of all knowledge',
-            url: 'https://www.google.ca',
+            title: 'Purpose of Life?',
+            description: 'Thought provoking post',
+            url: 'https://medium.com/personal-growth/the-purpose-of-life-isnt-to-be-happy-a2b2ff16de3c',
             user_id: 1,
-            image: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_white_background_color_272x92dp.png'
+            image: 'https://cdn-images-1.medium.com/max/1200/1*s70u5rGeX4xMnopIxZTDzA.jpeg'
           }),
         knex('resources').insert(
           {
-            title: 'Stack the knowledge',
-            description: 'Only website a dev needs',
-            url: 'https://www.stackoverflow.com',
+            title: 'The Don!',
+            description: 'What a beauty',
+            url: 'http://www.sportsnet.ca/baseball/mlb/don-cherry-changes-lyrics-take-ball-game-wrigley/',
             user_id: 2,
-            image: 'https://cdn.sstatic.net/Sites/stackoverflow/img/apple-touch-icon@2.png?v=73d79a89bded'
+            image: 'http://assets3.sportsnet.ca/wp-content/uploads/2017/08/cherry_don_cubs.jpg'
           }),
         knex('resources').insert(
           {
-            title: 'Dev Factory',
-            description: 'Anyone can make a baby, it takes LHL to make a developer',
-            url: 'https://www.lighthouselabs.ca',
+            title: 'Questions to ask about your startup',
+            description: 'Decent suggestions regarding startup considerations',
+            url: 'https://austinstartups.com/is-your-start-up-idea-gold-or-goop-40d86b3f009d',
             user_id: 3,
-            image: 'https://www.lighthouselabs.ca/assets/home-1b1053e3e6e74130233e7e45af571c266e4bc17df186390ff707c05194f90.jpg'
+            image: 'https://cdn-images-1.medium.com/max/1200/1*N-6vdg-eP1w_5Femejg5pA.jpeg'
+          }),
+        knex('resources').insert(
+          {
+            title: 'AI & Small Biz',
+            description: '',
+            url: 'https://www.fastcompany.com/40405996/how-small-businesses-can-harness-the-power-of-ai',
+            user_id: 1,
+            image: 'https://assets.fastcompany.com/image/upload/w_1280,f_auto,q_auto,fl_lossy/wp-cms/uploads/2017/04/Salesforce-Poster.jpg'
           }),
         knex('resource_categories').insert(
           {
